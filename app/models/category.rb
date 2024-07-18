4c846faafc53e0fab34b28860f1f2f970bf61b93
Godfrey Ndungu <godmak2022@gmail.com>
2024-07-18 13:01:43 +0300

    Add categories list,new and edit

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,14 @@
 class Category < ApplicationRecord
   has_many :articles
+  validates :name, presence: true, uniqueness: true, length: { in: 4..32 }
+  validates :slug, presence: true, uniqueness: true
+
+  before_validation :generate_slug
+
+  private
+
+  def generate_slug
+    self.slug = name.parameterize if name.present?
+  end
 
 end
