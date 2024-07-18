a616719cfca1e26f04cd239eba3062425d21b810
Godfrey Ndungu <godmak2022@gmail.com>
2024-07-19 01:53:00 +0300

    Add auth token email verification

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,21 @@ class User < ApplicationRecord
   has_one :author
 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
 
   has_many :articles, foreign_key: 'author_id'
   has_many :article_bodies, through: :articles
   has_many :comments, dependent: :destroy
   has_many :article_likes, dependent: :destroy
   has_many :liked_articles, through: :article_likes, source: :article
+
+  before_create :generate_registration_token
+
+  def generate_registration_token
+    self.registration_token = SecureRandom.hex(10)
+  end
+
+  def send_registration_email
+    UserMailer.registration_token(self).deliver_now
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_07_18_220809) do
+ActiveRecord::Schema.define(version: 2024_07_18_224320) do
 
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
@@ -147,6 +147,7 @@ ActiveRecord::Schema.define(version: 2024_07_18_220809) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "active", default: true
+    t.string "registration_token"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
