2af8763114b8bd4cbdac186754a3dda6658d54a7
Godfrey Ndungu <godmak2022@gmail.com>
2024-07-19 10:34:28 +0300

    Refactor article show and index using tailwindcss

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,22 +2,24 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def create
     build_resource(sign_up_params)
 
-    resource.save
-    yield resource if block_given?
-    if resource.persisted?
+    if resource.save
       if resource.active_for_authentication?
-        resource.send_registration_email
-        set_flash_message! :notice, :signed_up
-        sign_up(resource_name, resource)
-        respond_with resource, location: after_sign_up_path_for(resource)
+        resource.send_confirmation_instructions if resource.respond_to?(:send_confirmation_instructions)
+        if resource.active_for_authentication?
+          set_flash_message! :notice, :signed_up
+          sign_up(resource_name, resource)
+          respond_with resource, location: after_sign_up_path_for(resource)
+        else
+          expire_data_after_sign_in!
+          respond_with resource, location: after_inactive_sign_up_path_for(resource)
+        end
       else
+        clean_up_passwords resource
         set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
-        expire_data_after_sign_in!
-        respond_with resource, location: after_inactive_sign_up_path_for(resource)
+        respond_with resource, location: after_sign_up_path_for(resource)
       end
     else
       clean_up_passwords resource
-      set_minimum_password_length
       respond_with resource
     end
   end
